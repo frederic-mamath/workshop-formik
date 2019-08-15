d68332ac1b1013db5e4801d2d3dbb024206a2499
Frederic MAMATH <fredericmamath@gmail.com>
2019-08-15 13:37:08 +0200

    :construction: Add templates

---
+++ plop-templates/component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './{{ name }}.scss';
+
+type {{ name }}Props = {};
+
+type Props = {{ name }}Props;
+
+const {{ name }} = ({  }: Props) => <div className={styles.view} />;
+
+export default {{ name }};
+
